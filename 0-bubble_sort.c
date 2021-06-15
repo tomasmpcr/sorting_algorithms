56a02581842aaaefbed1b04c1427e70f541437f6
mstom332 <tomas@332.com.uy>
2021-06-15 17:21:05 +0000

    add ej 0

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -12,9 +12,12 @@ void bubble_sort(int *array, size_t size)
 	size_t i, j;
 	int num;
 
+	if (size < 2)
+		return;
+
 	for (i = 0; i < size; i++)
 	{
-		for (j = 0; j < size - i; j++)
+		for (j = 0; j < size; j++)
 		{
 			num = array[j];
 			if ((j + 1) <= size && num > array[j + 1])
