78a58f14ca0a32a01229148ce09e9c772f7c9343
mstom332 <tomas@332.com.uy>
2021-06-15 13:35:37 +0000

    add ej 0

---
+++ 0-bubble_sort.c
@@ -0,0 +1,28 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - This function order the array
+ * @array: The array to order
+ * @size: Length the array
+ * ----------------------------------------------
+ * Return: void
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j;
+	int num;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size - i; j++)
+		{
+			num = array[j];
+			if ((j + 1) <= size && num > array[j + 1])
+			{
+				array[j] = array[j + 1];
+				array[j + 1] = num;
+				print_array(array, size);
+			}
+		}
+	}
+}
